5b9310a4df395d0f98fbfa4cf7954fdbd5b6386a
nataliacadastros <natalialimaprofissionaliza@gmail.com>
2026-05-22 14:59:18 -0300

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useEffect } from "react";
+import { supabase } from "@/lib/supabase";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  redirect("/login");
+  const router = useRouter();
+
+  useEffect(() => {
+    async function verificar() {
+      const { data } = await supabase.auth.getSession();
+
+      if (!data.session) {
+        router.push("/login");
+      } else {
+        router.push("/gerenciamento");
+      }
+    }
+
+    verificar();
+  }, [router]);
+
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-[#0b0e1e] text-cyan-300">
+      <div className="font-black">CARREGANDO...</div>
+    </main>
+  );
 }
