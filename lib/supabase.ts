2d1dca178a5e61ee45b6efba19db2ae3e440205b
nataliacadastros <natalialimaprofissionaliza@gmail.com>
2026-05-22 15:13:42 -0300

    supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://akcwlxkpsdeudinpoyfd.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrY3dseGtwc2RldWRpbnBveWZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyNTc3ODAsImV4cCI6MjA5NDgzMzc4MH0.WQU5_9MbcNLZFsQuNiOO4ickquENcLmqAQuCx7S91L0";
+const supabaseAnonKey = "sb_publishable_1O5aoPpplgyOw3viYzlYBQ_pIygGTuX";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
