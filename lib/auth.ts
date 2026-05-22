b0a4112322c6d9ffa3fcb86791d6c26a28e9110d
nataliacadastros <natalialimaprofissionaliza@gmail.com>
2026-05-22 11:46:58 -0300

    auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,10 @@
+import { supabase } from "@/lib/supabase";
+
+export async function login(email: string, password: string) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  return { data, error };
+}
